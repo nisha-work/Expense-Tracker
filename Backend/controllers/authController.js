7a76be8bea223b3dec00efb92d118328617ae730
nisha-work <bscit.nisha123@gmail.com>
2025-11-10 22:43:24 +0530

    monday-10

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/User')
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 //generate jwt token
@@ -33,7 +33,7 @@ exports.registerUser = async (req, res) => {
             id: user._id,
             user,
             token: generateToken(user._id),
-        });
+        });    
     } catch (err) {
         res
         .status(500)
@@ -42,7 +42,42 @@ exports.registerUser = async (req, res) => {
 };
 
 //login user
-exports.loginUser = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) { 
+    return res.status(400).json({ message: "All fields are required" });
+}
+try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+        id: user._id,
+        user,
+        token: generateToken(user._id),
+    });
+} catch (err) {
+    res
+    .status(500)
+    .json({message: "Error registering user", error: err.message });
+}
+};
 
 //get user info 
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json(user);
+    } catch (err) {
+        res
+        .status(500)
+        .json({ message: "Error registering user", error: err.message });
+    }
+};
