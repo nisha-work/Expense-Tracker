7ba56cdb1b3ddd1bf656668502fdbfe465ae4000
nisha-work <bscit.nisha123@gmail.com>
2025-11-11 23:13:19 +0530

    heyy

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.protect = async (req, res, next) => {
-    let token = req.headers.authorization?.split(" ")[1];
+    let token = req.headers.authorization?.split(" ") [1];
     if (!token) return res.status(401).json({ message: "Not authorized, no token" });
 
     try {
--- Backend/server.js
+++ Backend/server.js
@@ -4,8 +4,13 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 
 const app = express();
+
+console.log("authRoutes type:", typeof authRoutes);
+console.log("incomeRoutes type:", typeof incomeRoutes);
+
 
 //middleware to handle cors
 app.use(
@@ -20,7 +25,11 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+
+//server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port  ${PORT}`));
